03012d0ab45276d162c29f1e2db293ebdeac3272
liu yi <lilugirl2005@126.com>
2021-11-24 21:04:03 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useContext} from "react";
+import "./App.css";
+import { useLocalStore, useObserver } from "mobx-react";
+
+const StoreContext = React.createContext();
+
+const StoreProvider = ({ children }) => {
+  const store = useLocalStore(() => ({
+    bugs: ["Centipede"],
+    addBug: (bug) =>{
+      store.bugs.push(bug)
+    },
+    get bugsCount() {
+      return store.bugs.length;
+    }
+  }));
+  return (
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  );
+};
+
+const BugsHeader = () =>{
+  const store = useContext(StoreContext);
+  return useObserver(()=>(<h1>{store.bugsCount} Bugs!</h1>))
+}
+
+const BugsList = () => {
+  const store = React.useContext(StoreContext);
+  return useObserver(()=>(
+    <ul>
+      {store.bugs.map((bug, index) => (
+        <li key={index}>{bug}</li>
+      ))}
+    </ul>
+  ))
+};
+
+const BugsForm = () =>{
+  const store = useContext(StoreContext);
+  const [bug, setBug]=useState('')
+  console.log(store)
+  return <form onSubmit={(e)=>{
+    
+    store.addBug(bug);
+    setBug('')
+    e.preventDefault();
+
+  }}>  
+  <input placehoder="bugs" value={bug} onChange={(e)=>setBug(e.target.value)} />
+  <button>Add</button>
+  </form>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <main>
+        <BugsHeader />
+        <BugsForm/>
+        <BugsList />
+      </main>
+    </StoreProvider>
   );
 }
 
